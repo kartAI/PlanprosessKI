62d6a9176a2c2c15560867f2c19063fd5ac91fe5
Maja Solberg Petterson <144062260+majskolbe@users.noreply.github.com>
2026-03-11 13:04:38 +0100

    Update app.py

--- versjonsoversikt-poc5/backend/app.py
+++ versjonsoversikt-poc5/backend/app.py
@@ -3,19 +3,12 @@ import os
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from services.diff_analyse import analyse_all_diff
-
-from flask import Flask, request, jsonify, send_from_directory
-from flask_cors import CORS
 from werkzeug.utils import secure_filename
-import os
 from pathlib import Path
 
 
 app = Flask(__name__)
 CORS(app)
-
-if __name__ == "__main__":
-    app.run(debug=True)
 
 UPLOAD_FOLDER = Path(__file__).parent / "uploads"
 UPLOAD_FOLDER.mkdir(exist_ok=True)
